9219d771bed84b67421167133f98e6aec62cb415
Piero Sabino <piero.sa@icloud.com>
2021-12-12 19:16:10 +0100

    Add Remove button in the cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,13 +2,12 @@
 import { Link } from "react-router-dom";
 
 const Cart = () => {
- // Function to remove all products from cart with the api call
-  const removeAllProducts = () => {
+ 
 
   return (
     <>
       <h1>Cart</h1>
-      <button onClick={clearCart}>Remove all</button>
+      <button >Remove all</button>
       <div className="total-cart">Total cart</div>
 
       <Link to="/checkout" ><button className="cart-checkout">Checkout</button></Link>
